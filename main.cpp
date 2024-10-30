4a8fc53f0ca73701d4468e041f7af77bb3a4e5ab
HXPPY228 <stetsurin.elisey@gmail.com>
2024-10-31 00:28:16 +0300

    version 1.2 | pictures added to the project itself

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("Dota 2 Drafter");
-    w.setWindowIcon(QIcon("D:/BSUIR/Kursovaya/Pictures/Dota2Drafter.jpg"));
+    w.setMinimumSize(w.size());
+    w.setWindowIcon(QIcon(":/img/Pictures/Dota2Drafter.jpg"));
     w.show();
     return a.exec();
 }
